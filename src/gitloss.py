a983dc8699dfbdfff16408efa2aedc20832c64a8
Kamran Janjua <kamranejaz98@gmail.com>
2018-04-24 02:38:49 +0500

    Create gitloss.py

---
+++ src/gitloss.py
@@ -0,0 +1,248 @@
+'''
+MIT License
+
+Copyright (c) 2018 shah, janjua
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+import os, tflearn
+import numpy as np
+import tensorflow as tf
+from tflearn.datasets import cifar10
+from tensorflow.examples.tutorials.mnist import input_data
+import matplotlib as mlp 
+mlp.use('Agg')
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+import itertools, math
+import tensorflow.contrib.layers as initializers
+
+# Constants 
+k = 1 # for harmonic mean
+threshold = 0.4
+range_val = 2
+LAMBDA = 1.0
+CENTER_LOSS_ALPHA = 0.5
+NUM_CLASSES = 10
+epoch = 0
+counter = 0
+checkpoint_dir = ""
+slim = tf.contrib.slim
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+val_list = [1, 0.1, 0.01, 0.001, 0.0001, 1e-4, 1e-6, 1e-8]
+
+with tf.name_scope('input'):
+    input_images = tf.placeholder(tf.float32, shape=(None,28,28,1), name='input_images')
+    labels = tf.placeholder(tf.int64, shape=(None), name='labels')
+
+# changing values over time. 
+global_step = tf.Variable(0, trainable=False, name='global_step')
+value_factor = tf.placeholder(tf.float32, shape=(None), name='value_factor')
+
+
+def git_loss(features, labels, alpha, num_classes, value_factor):
+    len_features = features.get_shape()[1]
+    centers = tf.get_variable('centers', [num_classes, len_features], dtype=tf.float32,
+                              initializer=tf.constant_initializer(0), trainable=False)
+    labels = tf.reshape(labels, [-1])
+    centers_batch = tf.gather(centers, labels)
+
+    loss = tf.reduce_mean(tf.square(features - centers_batch))
+
+    # Pairwise differences
+    diffs = (features[:, tf.newaxis] - centers_batch[tf.newaxis, :])
+    diffs_shape = tf.shape(diffs)
+
+    # Mask diagonal (where i == j)
+    mask = 1 - tf.eye(diffs_shape[0], diffs_shape[1], dtype=diffs.dtype)
+    diffs = diffs * mask[:, :, tf.newaxis]
+
+    # combinaton of two losses
+    loss2 = tf.reduce_mean(tf.divide(1, 1 + tf.square(diffs))) # margin. 
+
+    diff = centers_batch - features
+    unique_label, unique_idx, unique_count = tf.unique_with_counts(labels)
+    appear_times = tf.gather(unique_count, unique_idx)
+    appear_times = tf.reshape(appear_times, [-1, 1])
+
+    diff = tf.divide(diff, tf.cast((1 + appear_times), tf.float32))
+    diff = alpha * diff
+
+    centers_update_op = tf.scatter_sub(centers, labels, diff)  # diff is used to get updated centers.
+
+    # combo_loss = value_factor * loss + new_factor * loss2
+    combo_loss = loss + loss2
+
+    return combo_loss, centers_update_op
+
+def get_center_loss(features, labels, alpha, num_classes, value_factor):
+    len_features = features.get_shape()[1]
+    centers = tf.get_variable('centers', [num_classes, len_features], dtype=tf.float32,
+        initializer=tf.constant_initializer(0), trainable=False)
+    labels = tf.reshape(labels, [-1])
+    centers_batch = tf.gather(centers, labels)    
+    # Enhanced Center Loss
+    pairwise_differences = features[:, tf.newaxis] - centers_batch[tf.newaxis, :] # does the pair wise. col vector - row vector. 
+    print("Features: ", features[:, tf.newaxis])
+    cos_sim = tf.losses.cosine_distance(features[:, tf.newaxis], centers_batch[tf.newaxis, :])
+    cos_loss = tf.reduce_mean(cos_sim)
+
+    pairwise_differences_shape = tf.shape(pairwise_differences)
+    print("Shape: ", pairwise_differences_shape)
+
+    mask = 1 - tf.eye(pairwise_differences_shape[0], pairwise_differences_shape[1], dtype=pairwise_differences.dtype)
+    pairwise_differences = pairwise_differences * mask[:, :, tf.newaxis] # change shape of 
+    pairwise_loss = tf.square(tf.nn.l2_loss(pairwise_differences)) # squaring the pairwise_loss
+    #new_loss  = value_factor*loss + (k * pairwise_loss) # harmonic mean of pairwise loss
+    new_loss = k * pairwise_loss # correct implementation. 
+    
+    return cos_loss, centers_batch
+
+def inference(input_images):
+    with slim.arg_scope([slim.conv2d], kernel_size=3, padding='SAME'):
+        with slim.arg_scope([slim.max_pool2d], kernel_size=2):
+            x = slim.conv2d(input_images, num_outputs=32, weights_initializer=initializers.xavier_initializer(), scope='conv1_1')
+            x = slim.conv2d(x, num_outputs=32, weights_initializer=initializers.xavier_initializer(), scope='conv1_2')
+            x = slim.max_pool2d(x, scope='pool1')
+            x = slim.conv2d(x, num_outputs=64, weights_initializer=initializers.xavier_initializer(), scope='conv2_1')
+            x = slim.conv2d(x, num_outputs=64, weights_initializer=initializers.xavier_initializer(), scope='conv2_2')
+            x = slim.max_pool2d(x, scope='pool2')
+            x = slim.conv2d(x, num_outputs=128, weights_initializer=initializers.xavier_initializer(), scope='conv3_1')
+            x = slim.conv2d(x, num_outputs=128, weights_initializer=initializers.xavier_initializer(), scope='conv3_2')
+            x = slim.max_pool2d(x, scope='pool3')
+            x = slim.flatten(x, scope='flatten')
+            feature = slim.fully_connected(x, num_outputs=2, activation_fn=None, scope='fc1')
+            x = tflearn.prelu(feature)
+            x = slim.fully_connected(x, num_outputs=10, activation_fn=None, scope='fc2')
+    return x, feature
+
+def build_network(input_images, labels, value_factor, ratio=0.5):
+    logits, features = inference(input_images)
+    with tf.name_scope('loss'):
+        with tf.name_scope('mod_center_loss'):
+            git_loss, centers_update_op = git_loss(features, labels, CENTER_LOSS_ALPHA, NUM_CLASSES, value_factor)
+        with tf.name_scope('softmax_loss'):
+            softmax_loss = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(labels=labels, logits=logits))
+        with tf.name_scope('total_loss'):
+            total_loss = softmax_loss + ratio * git_loss
+    
+    with tf.name_scope('acc'):
+        accuracy = tf.reduce_mean(tf.cast(tf.equal(tf.arg_max(logits, 1), labels), tf.float32))
+    
+    with tf.name_scope('loss/'):
+        tf.summary.scalar('ModCenterLoss', mod_center_loss)
+        tf.summary.scalar('SoftmaxLoss', softmax_loss)
+        tf.summary.scalar('TotalLoss', total_loss)
+        
+    return logits, features, total_loss, accuracy, centers_update_op # returns total loss
+
+logits, features, total_loss, accuracy, centers_update_op = build_network(input_images, labels, value_factor, ratio=LAMBDA)
+# var = [v for v in tf.trainable_variables() if v.name == "tower_2/filter:0"][0] gets the weight of the desired layer.
+mnist = input_data.read_data_sets('/tmp/mnist', reshape=False)
+optimizer = tf.train.AdamOptimizer(0.001)
+with tf.control_dependencies([centers_update_op]):
+    train_op = optimizer.minimize(total_loss, global_step=global_step)
+summary_op = tf.summary.merge_all()
+sess = tf.Session()
+sess.run(tf.global_variables_initializer())
+saver = tf.train.Saver()
+writer = tf.summary.FileWriter('/tmp/mnist_log', sess.graph)
+step = sess.run(global_step) + 1 # so that the start is from 1 and not from 0.
+mean_data = np.mean(mnist.train.images, axis=0)
+while step < 8000:
+    if step % 1000 == 0: 
+        counter += 1
+        batch_images, batch_labels = mnist.train.next_batch(128)
+        _, summary_str, train_acc, train_loss = sess.run(
+            [train_op, summary_op, accuracy, total_loss],
+            feed_dict={
+                input_images: batch_images - mean_data,
+                labels: batch_labels,
+                value_factor: val_list[counter]
+            })
+    else:
+        batch_images, batch_labels = mnist.train.next_batch(128)
+        _, summary_str, train_acc, train_loss = sess.run(
+            [train_op, summary_op, accuracy, total_loss],
+            feed_dict={
+                input_images: batch_images - mean_data,
+                labels: batch_labels,
+                value_factor: val_list[counter]
+            })
+    step += 1
+    writer.add_summary(summary_str, global_step=step) 
+    print(("Step: {}, Loss: {:.4f}".format(step, train_loss))) #prints training loss and steps. 
+    if step % 200 == 0:
+        epoch += 1
+        vali_image = mnist.validation.images - mean_data
+        vali_acc = sess.run(
+            accuracy,
+            feed_dict={
+                input_images: vali_image,
+                labels: mnist.validation.labels
+            })
+        print("\nEpochs Done: {}".format(epoch))
+        print("{} Steps Done.".format(step))
+        print(("Step: {}, Train_Acc:{:.4f}, Valid_Acc:{:.4f}".
+              format(step, train_acc, vali_acc)))
+        print("\n")
+        print("==================================================================")
+        li = (sess.run(centers_update_op, feed_dict={
+            input_images: batch_images - mean_data,
+            labels: batch_labels,
+        }))
+        feat_samp = (sess.run(features, feed_dict={
+            input_images: batch_images - mean_data,
+            labels: batch_labels,
+        }))
+        compute_d2(li, feat_samp)
+        n = [0,1,2,3,4,5,6,7,8,9]
+        save_path = saver.save(sess, "model-epoch{}.ckpt".format(epoch))
+        print("Model saved for epoch # {}".format(epoch))
+        print("Plot at epoch # {}".format(epoch))
+        fig, ax = plt.subplots()
+        for i, txt in enumerate(n):
+            x,y = (li[i][0], li[i][1])
+            ax.annotate(txt, (x,y))
+            plt.scatter(x,y)
+            fig.savefig('PlotWithNewTest-{}.png'.format(epoch))
+        print("==================================================================")
+        print("\n")
+
+# Final Graphing       
+li = (sess.run(centers_update_op, feed_dict={
+            input_images: batch_images - mean_data,
+            labels: batch_labels,
+        }))
+# Graphing the end results.
+feat = sess.run(features, feed_dict={input_images:mnist.train.images[:10000]-mean_data})
+labels = mnist.train.labels[:10000]
+
+f = plt.figure(figsize=(16,9))
+c = ['#ff0000', '#ffff00', '#00ff00', '#00ffff', '#0000ff', 
+     '#ff00ff', '#990000', '#999900', '#009900', '#009999']
+for i in range(10):
+    plt.plot(feat[labels==i,0].flatten(), feat[labels==i,1].flatten(), '.', c=c[i])
+plt.scatter(*zip(*li)) # To plot the centers on the datasets. 
+plt.legend(['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'])
+plt.grid()
+plt.show()
+f.savefig('clustered_plot.png')
+
+# Distance Computing. 
